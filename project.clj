21058db5c4a0390478d92317318c1a4ba7f5fde1
Alex Osborne <ato@meshy.org>
2009-11-16 18:15:37 +1100

    Use JSch directly rather than ant-jsch so we can pickup identities and show errors.

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
 (defproject lein-clojars "0.5.0-SNAPSHOT"
   :description "Leiningen plugin for interacting with Clojars.org."
-  :dependencies [
-                 ;[leiningen "0.5.0-SNAPSHOT"]
-                 [com.jcraft/jsch "0.1.42"]
-                 [ant/ant-jsch "1.6.2"]])
+  :dependencies [[com.jcraft/jsch "0.1.42"]])
--- src/leiningen/push.clj
+++ src/leiningen/push.clj
@@ -1,41 +1,78 @@
 (ns leiningen.push
   (:require [lancet])
-  (:use [leiningen.deps :only [deps-if-missing]]
+  (:use [clojure.contrib.java-utils :only [as-file]]
+        [clojure.contrib.duck-streams :only [copy]]
+        [leiningen.deps :only [deps-if-missing]]
         [leiningen.jar :only [jar]]
         [leiningen.pom :only [pom]])
-  (:import (com.jcraft.jsch JSch JSchException)
-           (java.io File)))
+  (:import (com.jcraft.jsch JSch JSchException Logger)
+           (java.io File FileInputStream)))
+
+(let [re-repo #"(?:([^@]+)@)?([^:]+)(?::(\d+))?(?::(.*))?"]
+  (defn parse-repo [repo]
+    (let [[_ user host port path] (re-matches re-repo 
+                                              (or repo "clojars@clojars.org"))]
+      [(or user (System/getProperty "user.name")) 
+       host
+       (if port (Integer/parseInt port) 22)
+       (or path ".")])))
 
 (defn- add-identities [jsch]
  (let [homedir (File. (System/getProperty "user.home"))
        leindir (File. homedir ".leiningen")
-       sshdir (File. homedir ".ssh")
-       jsch (JSch.)]
+       sshdir (File. homedir ".ssh")]
    (doseq [dir [leindir sshdir]
            name ["id_rsa" "id_dsa" "identity"]
            :let [file (File. dir name)]
-           :when (.exists file)]
+           :when (do (println file) (.exists file))]
      (try
       (.addIdentity jsch (str file))
+      (println "added identity" (str file))
       (catch JSchException e
         (println "Skipping invalid key" (str file)))))))
 
+(defn- read-ack [in]
+  (let [b (.read in)]
+    (when-not (zero? b)
+      (throw (Exception. (str "scp expected ACK but got " b))))))
+
+(defn scp-send [repo files]
+  (let [jsch (doto (JSch.) (add-identities))
+        [user host port path] (parse-repo repo)
+        session (doto (.getSession jsch user host port)
+                  (.setConfig "StrictHostKeyChecking" "no")
+                  (.connect))
+        channel (doto (.openChannel session "exec")
+                  (.setCommand (str "scp -p -t " \" path \"))
+                  (.setErrStream System/err true))
+        in (.getInputStream channel)
+        out (.getOutputStream channel)]
+    (try
+     (.connect channel)
+     (read-ack in)
+     (doseq [path files]
+       (let [file (as-file path)]
+         (.write out (.getBytes (str "C0644 " (.length file) " "
+                                     (.getName file) "\n")))
+         (.flush out)
+         (read-ack in)
+
+         (copy (FileInputStream. file) out)
+         (.write out 0)
+         (read-ack in)))
+     (.close out)
+     (.read in) ; wait for remote close
+     (finally
+      (.disconnect channel)
+      (.disconnect session)))))
 
 (defn push
-  "Push a jar to the Clojars.org repository"
+  "Push a jar to the Clojars.org repository over scp"
   [project & [repo]]
+  (JSch/setLogger (proxy [Logger] []
+                  (isEnabled [level] true)
+                  (log [level message] (println level message))))
   (let [jarfile (str (:root project) "/" (:name project) ".jar")]
     (pom project)
     (jar project)
-    (doto (Scp.)
-      (.setProject lancet/ant-project)
-      (.setVerbose true)
-      (.setPassphrase "")
-      (.setTodir "clojars@clojars.org:")
-      (.setTrust true)
-      (.setKeyfile "/home/ato/.ssh/id_rsa")
-      (.addFileset
-       (lancet/fileset {:dir "/tmp/baz"}))
-      (.execute))))
-
-
+    (scp-send repo jarfile)))
